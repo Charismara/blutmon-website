636f25b2835df674da2d9952e7c35ea8980f56fc
Charismara <craoulis@gmail.com>
2020-09-12 00:51:01 +0200

    Add Images to game cards

--- src/components/GameSection/GameSection.js
+++ src/components/GameSection/GameSection.js
@@ -6,8 +6,8 @@ const gameSection = props => {
     return <div className="row">
         <div className="col">
             <ul className="section">
-                <GameSectionCard imgSrc="comming soon" title="Guild Wars 2" line1="Usere Server: Dzagonur [DE]" line2="Gildenleader: hunter.7123" line3="" />
-                <GameSectionCard imgSrc="comming soon" title="Minecraft" line1="GameMaster: MrProfessor" line2="Support: Rizetrabzon" line3="" />
+                <GameSectionCard imgSrc="gw2_icon_resized.png" title="Guild Wars 2" line1="Usere Server: Dzagonur [DE]" line2="Gildenleader: hunter.7123" line3="" />
+                <GameSectionCard imgSrc="mc_logo.webp" title="Minecraft" line1="GameMaster: MrProfessor" line2="Support: Rizetrabzon" line3="" />
             </ul>
         </div>
     </div>
--- src/components/GameSectionCard/GameSectionCard.js
+++ src/components/GameSectionCard/GameSectionCard.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './GameSectionCard.css'
 
 const gameSectionCard = props => {
-    let img = <img src={props.imgSrc} alt="placerholder_image"></img>
-    let title = <h5>{props.title}</h5>
-    let line1 = <p>{props.line1}</p>
-    let line2 = <p>{props.line2}</p>
-    let line3 = <p>{props.line3}</p>
+    let imgSrc = props.imgSrc;
+    let img = <img src={require('../../images/' + imgSrc)} />;
+    let title = <h5>{props.title}</h5>;
+    let line1 = <p>{props.line1}</p>;
+    let line2 = <p>{props.line2}</p>;
+    let line3 = <p>{props.line3}</p>;
 
     return <li className="card">
         {img}
