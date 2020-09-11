3e9cd49d8d1e3baa1f464a939c39c19ae175955d
Charismara <craoulis@gmail.com>
2020-09-12 01:53:22 +0200

    resize images in cards

--- src/components/GameSection/GameSection.js
+++ src/components/GameSection/GameSection.js
@@ -3,9 +3,6 @@ import './GameSection.css';
 import GameSectionCard from '../GameSectionCard/GameSectionCard';
 
 const gameSection = props => {
-    let rowCount = 3;
-
-
     return <div className="row">
         <div className="col">
             <ul className="section">
@@ -16,16 +13,16 @@ const gameSection = props => {
                     <GameSectionCard imgSrc="mc_logo.webp" title="Minecraft" line1="GameMaster: MrProfessor" line2="Support: Rizetrabzon" line3="" />
                 </li>
                 <li>
-                    <GameSectionCard imgSrc="league_icon.png" title="League of Legends" line1="Usere Server: EUW, NA" line2="Kontakt: Chαrιsmara" line3="" />
+                    <GameSectionCard imgSrc="league_icon_resized.png" title="League of Legends" line1="Usere Server: EUW, NA" line2="Kontakt: Chαrιsmara" line3="" />
                 </li>
                 <li>
-                <GameSectionCard imgSrc="discord_icon.png" title="Discord" line1="Admin: Professor#0815" line2="https://discord.gg/KSTSZsF" line3="" />
+                <GameSectionCard imgSrc="discord_icon_resized.png" title="Discord" line1="Admin: Professor#0815" line2="https://discord.gg/KSTSZsF" line3="" />
                 </li>
                 <li>
-                <GameSectionCard imgSrc="overwatch_icon.svg" title="Overwatch" line1="Usere Server: EU" line2="Kontakt: Charismara#2601" line3="" />    
+                <GameSectionCard imgSrc="overwatch_icon_resized.png" title="Overwatch" line1="Usere Server: EU" line2="Kontakt: Charismara#2601" line3="" />    
                 </li>
                 <li>
-                <GameSectionCard imgSrc="steam_icon.png" title="Steam" line1="Usere Gruppe: BlutmondGilde" line2="Leiter: Professor" line3="" />    
+                <GameSectionCard imgSrc="steam_icon_resized.png" title="Steam" line1="Usere Gruppe: BlutmondGilde" line2="Leiter: Professor" line3="" />    
                 </li>
             </ul>
         </div>
