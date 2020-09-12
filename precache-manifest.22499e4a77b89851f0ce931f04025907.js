43905195b49eea710f83c411aba8944de1f22bcf
Charismara <craoulis@gmail.com>
2020-09-12 02:37:27 +0200

    Updates

---
+++ precache-manifest.22499e4a77b89851f0ce931f04025907.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3afe10c95199877242e1f05325527f",
+    "url": "/blutmond-website/index.html"
+  },
+  {
+    "revision": "ac985b1cd539f50b798a",
+    "url": "/blutmond-website/static/css/main.80e7748f.chunk.css"
+  },
+  {
+    "revision": "aa047768129402a6054c",
+    "url": "/blutmond-website/static/js/2.06649645.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blutmond-website/static/js/2.06649645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac985b1cd539f50b798a",
+    "url": "/blutmond-website/static/js/main.c7a3944a.chunk.js"
+  },
+  {
+    "revision": "23a8bf5a57a76ba94583",
+    "url": "/blutmond-website/static/js/runtime-main.9cc53a86.js"
+  },
+  {
+    "revision": "9bbfaf439017002f40b0cbd91fb6b962",
+    "url": "/blutmond-website/static/media/blutmond_icon_v2.9bbfaf43.png"
+  },
+  {
+    "revision": "7a9c2f4bbdd2208389a7ac7e18f8ea01",
+    "url": "/blutmond-website/static/media/discord_icon.7a9c2f4b.png"
+  },
+  {
+    "revision": "702205bd960481d61d201363b4439db3",
+    "url": "/blutmond-website/static/media/gw2_icon.702205bd.png"
+  },
+  {
+    "revision": "c51365f435d6f5840735486454e67f5f",
+    "url": "/blutmond-website/static/media/gw2_icon_resized.c51365f4.png"
+  },
+  {
+    "revision": "059ada6006a1eca476309b6c5a1589d0",
+    "url": "/blutmond-website/static/media/league_icon.059ada60.png"
+  },
+  {
+    "revision": "afe77e9541b940f3ec2490551c55c6f8",
+    "url": "/blutmond-website/static/media/league_icon_resized.afe77e95.png"
+  },
+  {
+    "revision": "d8ea5984741fed08fdc747638910181b",
+    "url": "/blutmond-website/static/media/mc_logo.d8ea5984.webp"
+  },
+  {
+    "revision": "1a528d9e529911465757e1869e94ac1d",
+    "url": "/blutmond-website/static/media/overwatch_icon.1a528d9e.svg"
+  },
+  {
+    "revision": "2ec14bd97ab4337220b7e3ce38d2224f",
+    "url": "/blutmond-website/static/media/steam_icon.2ec14bd9.png"
+  }
+]);
